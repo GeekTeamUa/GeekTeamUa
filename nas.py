cdf46808bc2adec5cfa90cf624ab3537fe101332
viktor <viktor.com9988@gmail.com>
2018-03-16 18:27:26 +0200

    Update nas.py

--- nas.py
+++ nas.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import numpy as np
 import glob
-path = '/home/viktor/PycharmProjects/wqq/A_2'
 image_list = []
 for infile in glob.glob("/home/viktor/PycharmProjects/wqq/A_2/*.jpg"):
     img = Image.open(infile)
