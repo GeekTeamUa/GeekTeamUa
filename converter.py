4f4cbdd9a24386f553c2d084314c861c250b6566
Dmytrokain <Dimikowowik@gmail.com>
2018-03-17 16:36:20 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,21 +1,27 @@
 from PIL import Image
 import numpy
 import glob
+import os
+
+save_path = '/home/dmytro/PycharmProjects/script_convert_jpg_to_numpy/npy_data'
+
+data_folders = glob.glob('/home/dmytro/PycharmProjects/dataset/letters/*')
 
 
-filelist = glob.glob("/home/dmytro/PycharmProjects/dataset/letters/А_1/*")      # filelist didn't see A_1 folder, so i changed en. "A" to ukrainian "A".Lol.
+for folder in data_folders:
+    all_images_in_folder = glob.glob('/home/dmytro/PycharmProjects/dataset/letters/'+str(os.path.basename(folder))+"/*")
+    for img in all_images_in_folder:
+        im = Image.open(img).convert('L')  
+        (width, height) = im.size
+        greyscale_image = list(im.getdata())
+        greyscale_image = numpy.array(greyscale_image)
+        greyscale_image = greyscale_image.reshape((height, width))
+
+    # Save in folder(takes longer time)
+    # numpy.save(os.path.join(save_path, str(os.path.basename(folder))), greyscale_image)  
+   
+    # Save in current directory(works faster because of unknown reasons)
+    numpy.save(str(os.path.basename(folder)), greyscale_image)    
 
 
-all_images_in_folder = [i for i in filelist]   # Create list of all images in folder
-
-
-for img in all_images_in_folder:
-    im = Image.open(img).convert('L')         
-    (width, height) = im.size
-    greyscale_image = list(im.getdata())
-    greyscale_image = numpy.array(greyscale_image)
-    greyscale_image = greyscale_image.reshape((height, width))
-
-numpy.save("array_of_A1", greyscale_image)
-
-print(greyscale_image)
+# numpy.save("array", img)
