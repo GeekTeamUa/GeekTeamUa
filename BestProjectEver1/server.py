3b2848c52b35fa6b3de3f4774714c48faaf615b1
viktor <viktor.com9988@gmail.com>
2018-05-07 01:03:55 +0300

    web app v1.2.5

--- BestProjectEver1/server.py
+++ BestProjectEver1/server.py
@@ -24,45 +24,114 @@ def result():
     img = request.files['img']
     file = os.path.join(app.config['UPLOAD_FOLDER'], img.filename)
     img.save(file)
+    #
+    # basewidth = 28
+    # baseheight = 28
+    # img = Image.open(file)
+    #
+    # wpercent = (basewidth / float(img.size[0]))
+    # hsize = int((float(img.size[1]) * float(wpercent)))
+    # img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+    # hpercent = (baseheight / float(img.size[1]))
+    # wsize = int((float(img.size[0]) * float(hpercent)))
+    # img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+    # img_array = np.array(img)
 
-    basewidth = 28
-    baseheight = 28
-    img = Image.open(file)
-
-    wpercent = (basewidth / float(img.size[0]))
-    hsize = int((float(img.size[1]) * float(wpercent)))
-    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
-    hpercent = (baseheight / float(img.size[1]))
-    wsize = int((float(img.size[0]) * float(hpercent)))
-    img = img.resize((wsize, baseheight), Image.ANTIALIAS)
-    img_array = np.array(img)
-    img_array=img_array.reshape(1,28,28,1)
-    os.remove(file)
-
-    json_file = open('model.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    loaded_model.load_weights("model.h5")
-    loaded_model.compile(loss=keras.losses.categorical_crossentropy,
-                         optimizer=keras.optimizers.Adadelta(),
-                         metrics=['accuracy'])
-    res = loaded_model.predict_classes(img_array)
-
-   # print (res)
-    res_str=np.array_str(res)
-    res_str=res_str.replace("[","")
-    res_str = res_str.replace("]", "")
+    #os.remove(file)
 #    return render_template("index.html",img=res_str)
+
     if (select == "1"):
-        return render_template("index.html", final_text = "test1",img=res_str)
+        basewidth = 28
+        baseheight = 28
+        img = Image.open(file)
+        wpercent = (basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+        hpercent = (baseheight / float(img.size[1]))
+        wsize = int((float(img.size[0]) * float(hpercent)))
+        img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+        img_array = np.array(img)
+        img_array=img_array.reshape(1,28,28,1)
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights("model.h5")
+        loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
+        res = loaded_model.predict_classes(img_array)
+        res_str=np.array_str(res)
+        res_str=res_str.replace("[","")
+        res_str = res_str.replace("]", "")
+        return render_template("index.html", final_text = "mnist",img=res_str)
     if (select == "2") :
-        return render_template("index.html", final_text = "test2",img=res_str)
+        basewidth = 28
+        baseheight = 28
+        img = Image.open(file)
+        wpercent = (basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+        hpercent = (baseheight / float(img.size[1]))
+        wsize = int((float(img.size[0]) * float(hpercent)))
+        img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+        img_array = np.array(img)
+        img_array=img_array.reshape(1,28,28,1)
+        json_file = open('model.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights("model.h5")
+        loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adadelta(),
+        metrics=['accuracy'])
+        res = loaded_model.predict_classes(img_array)
+        res_str=np.array_str(res)
+        res_str=res_str.replace("[","")
+        res_str = res_str.replace("]", "")
+        return render_template("index.html", final_text = "ua-mnist",img=res_str)
     if (select == "3") :
-        return render_template("index.html", final_text = "test3",img=res_str)
+        basewidth = 28
+        baseheight = 28
+        img = Image.open(file)
+        wpercent = (basewidth / float(img.size[0]))
+        hsize = int((float(img.size[1]) * float(wpercent)))
+        img = img.resize((basewidth, hsize), Image.ANTIALIAS)
+        hpercent = (baseheight / float(img.size[1]))
+        wsize = int((float(img.size[0]) * float(hpercent)))
+        img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+        img_array = np.array(img)
+        img_array=img_array.reshape(1,28,28,1)
+        json_file = open('model3.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        loaded_model.load_weights("model3.h5")
+        loaded_model.compile(loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adadelta(),
+        metrics=['accuracy'])
+        res = loaded_model.predict_classes(img_array)
+        res_str=np.array_str(res)
+        res_str=res_str.replace("[","")
+        res_str = res_str.replace("]", "")
+        return render_template("index.html", final_text = "fashio-mnist",img=res_str)
 
 
-# @app.route('/upload', methods=['POST'])
+
+
+#    # print (res)
+#     res_str=np.array_str(res)
+#     res_str=res_str.replace("[","")
+#     res_str = res_str.replace("]", "")
+# #    return render_template("index.html",img=res_str)
+#     if (select == "1"):
+#         return render_template("index.html", final_text = "test1",img=res_str)
+#     if (select == "2") :
+#         return render_template("index.html", final_text = "test2",img=res_str)
+#     if (select == "3") :
+#         return render_template("index.html", final_text = "test3",img=res_str)
+#
+#
+# # @app.route('/upload', methods=['POST'])
 # def upload_file():
 #     file = request.files['image']
 #     f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
